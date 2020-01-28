19c9fd1041bb75bb4eedb864aa71c077896c09aa
Emil Ståhl <emil.stahl@icloud.com>
2020-01-28 15:16:08 +0100

    can now read from file into char buffer

---
+++ Homeworks/1. Critical Sections/3. tee/fileOps.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    char* buffer = 0;
+    long length;
+    FILE * f = fopen (argv[1], "rb"); //was "rb"
+
+    if (f)
+    {
+      fseek (f, 0, SEEK_END);
+      length = ftell (f);
+      fseek (f, 0, SEEK_SET);
+      buffer = (char*)malloc ((length+1)*sizeof(char));
+      if (buffer)
+      {
+        fread (buffer, sizeof(char), length, f);
+      }
+      fclose (f);
+    }
+    buffer[length] = '\0';
+    for (int i = 0; i < length; i++) {
+       printf("%c", buffer[i]);
+    }
+    printf("\n");
+    //printf("buffer = %s\n", buffer);
+
+}
