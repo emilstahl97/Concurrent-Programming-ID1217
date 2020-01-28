91efdfcf647a6e910a0708b56568f8d63e123e03
Emil Ståhl <emil.stahl@icloud.com>
2020-01-28 16:18:21 +0100

    works with threads if sleeping after read thread

--- Homeworks/1. Critical Sections/3. tee/fileOps.c
+++ Homeworks/1. Critical Sections/3. tee/fileOps.c
@@ -1,14 +1,15 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <pthread.h>
   
 long length;
 char* buffer = 0;
-FILE *fileRead, *fileWrite;
+FILE *fileToRead, *fileToWrite;
 
 
 void* writeToFile() {
 
-  fputs(buffer, fileWrite);
+  fputs(buffer, fileToWrite);
 
 }
 
@@ -22,17 +23,17 @@ void *printSTD() {
 
 void *readFile() {
 
-    if (fileRead)
+    if (fileToRead)
     {
-      fseek (fileRead, 0, SEEK_END);
-      length = ftell (fileRead);
-      fseek (fileRead, 0, SEEK_SET);
+      fseek (fileToRead, 0, SEEK_END);
+      length = ftell (fileToRead);
+      fseek (fileToRead, 0, SEEK_SET);
       buffer = (char*)malloc ((length+1)*sizeof(char));
       if (buffer)
       {
-        fread (buffer, sizeof(char), length, fileRead);
+        fread (buffer, sizeof(char), length, fileToRead);
       }
-      fclose (fileRead);
+      fclose (fileToRead);
     }
     buffer[length] = '\0';
 
@@ -40,12 +41,22 @@ void *readFile() {
 
 int main(int argc, char *argv[])
 {
-    fileRead = fopen (argv[1], "rb"); //was "rb"
-    fileWrite = fopen(argv[2], "w");
+    fileToRead = fopen (argv[1], "rb"); //was "rb"
+    fileToWrite = fopen(argv[2], "w");
+
+    pthread_t print, write, read;
 
     printf("Attempting to write to file\n");
+
+    pthread_create(&read, NULL, readFile, NULL);
+    sleep(1);
+    pthread_create(&print, NULL, printSTD, NULL);
+    pthread_create(&write, NULL, writeToFile, NULL);
+
+    pthread_join(read, NULL);
+    pthread_join(print, NULL);
+    pthread_join(write, NULL);
+    
+    pthread_exit(NULL);
   
-    readFile();
-    writeToFile();
-    printSTD();
 }
