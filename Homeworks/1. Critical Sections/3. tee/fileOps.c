31f06c8f5688b6aab22732bb52963e014365a2b5
Emil Ståhl <emil.stahl@icloud.com>
2020-01-28 15:46:27 +0100

    read file in seperate function

--- Homeworks/1. Critical Sections/3. tee/fileOps.c
+++ Homeworks/1. Critical Sections/3. tee/fileOps.c
@@ -1,17 +1,18 @@
 #include <stdlib.h>
 #include <stdio.h>
   
+long length;
 char* buffer = 0;
 FILE *f, *f2;
 
 
 void* writeToFile() {
 
-  int results = fputs(buffer, f2);
+  fputs(buffer, f2);
 
 }
 
-void *printSTD(int length) {
+void *printSTD() {
 
   for (int i = 0; i < length; i++) {
       printf("%c", buffer[i]);
@@ -19,11 +20,7 @@ void *printSTD(int length) {
   printf("\n");
 }
 
-int main(int argc, char *argv[])
-{
-    long length;
-    f = fopen (argv[1], "rb"); //was "rb"
-    f2 = fopen(argv[2], "w");
+void *readFile() {
 
     if (f)
     {
@@ -39,10 +36,16 @@ int main(int argc, char *argv[])
     }
     buffer[length] = '\0';
 
-    //printf("buffer = %s\n", buffer);
+}
+
+int main(int argc, char *argv[])
+{
+    f = fopen (argv[1], "rb"); //was "rb"
+    f2 = fopen(argv[2], "w");
 
     printf("Attempting to write to file\n");
-
+  
+    readFile();
     writeToFile();
-    printSTD(length);
+    printSTD();
 }
