932ffcdb8e40db07dce4cc0c8d77385bc90731e5
Emil Ståhl <emil.stahl@icloud.com>
2020-01-28 17:00:53 +0100

    seems to work now with lock and cond_wait

--- Homeworks/1. Critical Sections/3. tee/fileOps.c
+++ Homeworks/1. Critical Sections/3. tee/fileOps.c
@@ -5,16 +5,19 @@
 long length;
 char* buffer = 0;
 FILE *fileToRead, *fileToWrite;
-
+pthread_cond_t go;
+pthread_mutex_t lock;
 
 void* writeToFile() {
 
+  pthread_cond_wait(&go, &lock);
   fputs(buffer, fileToWrite);
 
 }
 
 void *printSTD() {
 
+  pthread_cond_wait(&go, &lock);
   for (int i = 0; i < length; i++) {
       printf("%c", buffer[i]);
     }
@@ -23,6 +26,7 @@ void *printSTD() {
 
 void *readFile() {
 
+    pthread_mutex_lock(&lock);
     if (fileToRead)
     {
       fseek (fileToRead, 0, SEEK_END);
@@ -37,19 +41,23 @@ void *readFile() {
     }
     buffer[length] = '\0';
 
+  pthread_cond_broadcast(&go);
+  pthread_mutex_unlock(&lock);
+
 }
 
 int main(int argc, char *argv[])
 {
     fileToRead = fopen (argv[1], "rb"); //was "rb"
     fileToWrite = fopen(argv[2], "w");
+    pthread_cond_init(&go, NULL);
+    pthread_mutex_init(&lock, NULL);
 
     pthread_t print, write, read;
 
     printf("Attempting to write to file\n");
 
     pthread_create(&read, NULL, readFile, NULL);
-    sleep(1);
     pthread_create(&print, NULL, printSTD, NULL);
     pthread_create(&write, NULL, writeToFile, NULL);
 
