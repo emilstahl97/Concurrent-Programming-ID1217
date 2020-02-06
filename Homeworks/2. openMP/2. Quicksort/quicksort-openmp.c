fc4f4ebae3dafe6acce620f1ff777895cda5b6b0
Emil Ståhl <emil.stahl@icloud.com>
2020-02-06 16:08:23 +0100

    works, I guess

--- Homeworks/2. openMP/2. Quicksort/quicksort-openmp.c
+++ Homeworks/2. openMP/2. Quicksort/quicksort-openmp.c
@@ -1,30 +1,15 @@
-/* Quicksort algorithm utilizing multiple POSIX threads
-
-    Features: works with both input file as well as generating an array with randomized values 
-
-    usage under Linux:
-            gcc quicksort.c -o quicksort -lpthread
-            ./quicksort <input file>
-                or
-            ./quicksort arraySize range
-*/
-
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <time.h>
 #include <omp.h>
+
 #define MAX_SIZE 1000
 #define MAX_RANGE 10000
-
+#define LOW_LIMIT 1000
 int arraySize, *array;
-struct Part {
-	int low;
-	int high;
-};
 
 void print(int [], int);
-void swap(int[], int, int);
-void *quicksort(void *struc);
-int partitioning(int low, int high);
 
 void print(int array[], int length) {
 	int i;
@@ -34,61 +19,73 @@ void print(int array[], int length) {
 	printf(" ]\n");
 }
 
-void swap(int array[], int left, int right) {
-	int temp;
-	temp = array[left];
-	array[left] = array[right];
-	array[right] = temp;
+
+/* When list gets small */
+static void insertionSort(int arr[], int n)
+{
+	int i, j;
+	float temp;
+	for (i = 1; i < n; i++)
+	{
+		temp = arr[i];
+		j = i - 1;
+
+		while (j >= 0 && arr[j] > temp)
+		{
+			arr[j + 1] = arr[j];
+			j = j - 1;
+		}
+		arr[j + 1] = temp;
+	}
 }
 
-int partitioning(int low, int high) {
+/* Meat of quick sort. Partitioning */
+static int partition(int pivot_index, int high, int *data)
+{
+	float pivot = data[pivot_index];
+	int k = pivot_index + 1;
+	int l = high;
+	float temp;
 
-    int pivot = array[high];
-    int wall = low - 1;
-    int current;
-
-    for(current = low; current <= high - 1; current++){
-        if(array[current] < pivot){
-            wall += 1;
-            swap(array, wall, current);
-        }
-    }
-    if(array[high] < array[wall + 1]){
-        swap(array, wall + 1, high);
-    }
-    return wall + 1;
+	while (k < l)
+	{
+		temp = data[k];
+		data[k] = data[l];
+		data[l] = temp;
+		while (data[k] <= pivot)
+			k++;
+		while (data[l] > pivot)
+			l--;
+	}
+	data[pivot_index] = data[l];
+	data[l] = pivot;
+	return l;
 }
 
-void* quicksort(void* struc){
-    struct Part * range = (struct Part *) struc;
-    int low = range->low;
-    int high = range->high;
+/* The parallel quicksort */
+static void quicksort(int pivot, int high, int *list)
+{
+	if (pivot >= high)
+		return;
 
-    if(low < high){
-        if(low < 0)
-            low = 0;
-        int p = partitioning(low, high);
-        struct Part left = {low, p - 1};
-        struct Part right = {p + 1, high};
+	if ((high - pivot) < LOW_LIMIT)
+		return insertionSort(&list[pivot], high - pivot);
 
-        #pragma omp task 
-        {
-        quicksort(&left);
-        }
-        //printf("nr of threads = %d\n", omp_get_num_threads());
-        #pragma omp task
-        {
-        quicksort(&right);
-        }  
-    }
-    return 0;
+	int mid = partition(pivot, high, list);
+
+#pragma omp task
+	quicksort(pivot, mid - 1, list);
+#pragma omp task
+	quicksort(mid + 1, high, list);
 }
+
+
 
 int main(int argc, char *argv[]) {
 
 	FILE *fh;
+    double start_time, end_time;  // start and end times
 	int data, range;
-    double start_time, end_time;
 
 	/* Initialize data. */
 	fh = fopen(argv[1], "r");
@@ -117,17 +114,12 @@ int main(int argc, char *argv[]) {
 	    printf("%d elements read\n", arraySize);
     }
 
-    struct Part partition = {0, arraySize - 1};
-
-    int numThreads = (argc > 3) ? atoi(argv[3]) :100;
-    omp_set_num_threads(numThreads);
-    
     printf("Sorting array...\n");
 
     start_time = omp_get_wtime();
-    quicksort(&partition);
+    quicksort(0, arraySize-1, array);
     end_time = omp_get_wtime();
-    
+
     printf("Sorted in %f ms \nSorted array:\n", (end_time - start_time)*1000);
 	print(array, arraySize);
 
