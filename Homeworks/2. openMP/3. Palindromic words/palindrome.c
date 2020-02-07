c3f2e3d85030f030c40aed2378eb43be5416a485
Emil Ståhl <emil.stahl@icloud.com>
2020-02-07 19:59:31 +0100

    merged test.c into palindrome.c, works

--- Homeworks/2. openMP/3. Palindromic words/palindrome.c
+++ Homeworks/2. openMP/3. Palindromic words/palindrome.c
@@ -1,151 +1,121 @@
+/*
+  finding palindromic words in a dictionary using openmp
+
+    usage with gcc (version 4.2 or higher required):
+      gcc -O -fopenmp -o palindromic palindromic.c
+      ./palindromic size numThreads
+*/
+
 #include <stdio.h>
+#include <ctype.h>
 #include <stdlib.h>
 #include <string.h>
-#include <ctype.h>
 #include <omp.h>
 
-#define MAX_WORDS 32000
-#define MAX_WORD_LENGTH 128
-#define MAX_THREADS 12
+#define MAXWORKERS 24
+#define WORDLENGTH 40
+#define TASKLENGTH 10
+#define MAXSIZE 30000
+#define MAX_THREADS 12 
 
+int sum = 0;
+int size;
+char dictionary[MAXSIZE][WORDLENGTH];
+int result_buffer[MAXSIZE];
+FILE* results;
+FILE* fileToRead;
+  
 
-int num_words = 0;
-int num_palindroms = 0;
-double start_time, end_time;
-
-char word_buff[MAX_WORD_LENGTH];
-char* dictionary[MAX_WORDS];
-
-/* Pointers to original strings in dictionary */
-char* found_palindroms[MAX_WORDS];
-
-/* Helper function */
-int is_palindrome(char* str1, char* str2, int length1, int length2);
-
-int main(int argc, char* argv[])
-{
-    int i=0, j=0, numThreads;
-    FILE* fileToRead;
-    FILE* results;
-
-    fileToRead = (argc > 1) ? fopen(argv[1], "r+") : fopen("./words.txt", "r+");
-    results = (argc > 2) ? fopen(argv[2], "w+") : fopen("./results.txt", "w+");
-    numThreads = (argc > 3) ? atoi(argv[3]) : MAX_THREADS;
-
-    omp_set_num_threads(numThreads);
-    
-    /* No file */
-    if (!fileToRead || !results) {
-        fprintf(stderr, "404 File not Found\n");
-        exit(1);
-    }
-
-    /* Load words */
-    while (fscanf(fileToRead, "%s", word_buff))
-    {
-        int word_length = strlen(word_buff);
-        dictionary[num_words] = malloc(sizeof(char)*word_length);
-        memcpy(dictionary[num_words++], word_buff, word_length);
-        
-        if (feof(fileToRead))
-            break;
-    }
-    fclose(fileToRead);
-
-    if(argc > 1)
-    printf("Searching for palindromes in file %s. . . .\n\n", argv[1]);
-
-    /* Record time */
-    start_time = omp_get_wtime();
-
-    /* Do stuff in parallell */
-#pragma omp parallel for private(i,j) schedule(dynamic,1)
-    for (i = 0; i < num_words; i++)
-    {
-        /* Got a word! Word is located at dictionary[i] */
-
-        int word_length_i = strlen(dictionary[i]);
-        int palindrome = 0;
-
-        /* Compare backwards */
-        if (is_palindrome(dictionary[i], dictionary[i], word_length_i, word_length_i))
-            palindrome = 1;
-
-        /* Compare to all following words */
-        for (j = i+1; j < num_words; j++)
-        {
-            if (is_palindrome(dictionary[i], dictionary[j], word_length_i, -1))
-            {
-                palindrome = 1;
-
-                /* Add the other palindrome */
-#pragma omp critical 
-                found_palindroms[num_palindroms++] = dictionary[j];
-
-                break;
-            }
-        }
-
-        /* Add the word examined as a palindrom */
-        if (palindrome) {
-#pragma omp critical
-            found_palindroms[num_palindroms++] = dictionary[i];
-        }
-    }
-
-    /* Done, save time and stuff */
-    end_time = omp_get_wtime();
-
-    /* Report to user */
-    printf("Found %d palindroms in %f seconds\n", num_palindroms, end_time-start_time);
-
-    /* Print result to file */
-    fprintf(results,"Found %d palindroms\n", num_palindroms);
-    i=0;
-    while(i < num_palindroms)
-        fprintf(results, "%s\n", found_palindroms[i++]);
-
-    /* Close outfile */
-    fclose(results);
-
-    return 0;
+/*
+    reverse string
+    char * word, string to reverse
+*/
+void reverse(char * word, char * r){
+  int i, j;
+  i = 0;
+  j = strlen(word) - 1;
+  while (j >= 0) {
+    r[i++] = word[j--];
+  }
+  r[i]  = '\0';
 }
 
-/* Check to c-style strings for palindimicity
- *
- * Lengths can be provided, at own risk, to increase performance by avoiding
- * calculating the lengths of strings more than once
- *
- * Returns 0 if not palindromic
- * Returns != 0 if palindromic
- *
- */
-int is_palindrome(char* str1, char* str2, int length1, int length2)
-{
-    /* If length was not provided, calculate */
-    if (length1 == -1)
-        length1 = strlen(str1);
-    if (length2 == -1)
-        length2 = strlen(str2);
-
-    /* Can't be palindomic if different lengths */
-    if (length1 == length2)
-    { 
-        int k;
-        for (k = 0; k < length2; k++)
-        {
-            char a = tolower(str1[k]);
-            char b = tolower(str2[length2-k-1]);
-
-            /* Check if reverse of i is the same as the current word */
-            if ((int)tolower(str1[k]) != (int)tolower(str2[length2-k-1]))
-                return 0;
-        }
-#ifdef DEBUG
-        printf("Seems like palindromic %s & %s\n", str1, str2);
-#endif
-        return 1;
-    }
+/*
+  binarySearch for searching in the dictionary
+*/
+int binarySearch(int l, int r, char * x){
+  while (l <= r){
+    int m = l + (r-l)/2;
+    int result = (int)strcmp(x, dictionary[m]);
+    if (result == 0)
+      return m;
+    if (result > 0)
+      l = m + 1;
     else
-        return 0; /* As in false */
+      r = m - 1;
+  }
+  return -1;
+}
+
+void Workers(int size){
+  int i, result;
+  #pragma omp parallel for reduction (+:sum)
+  for (i = 0; i < size; i++) {
+    // 1. Get word
+    char * word = dictionary[i];
+    // 2. flip word
+    char flip[WORDLENGTH];
+    reverse(word, flip);
+    // 3. search for fliped word in dictionary
+    result = binarySearch(i, size -1, flip);
+    // 4. if in dictionary, write to result file 
+    if(result != -1){
+      fprintf(results, "%s\n", dictionary[i]);
+      sum++;
+    }
+  }
+}
+
+int main(int argc, char *argv[]){
+  double start_time, end_time;
+  int k = 0, i, numThreads;
+  long l;
+  
+  fileToRead = (argc > 1) ? fopen(argv[1], "r+") : fopen("./words.txt", "r+");
+  results = (argc > 2) ? fopen(argv[2], "w+") : fopen("./results.txt", "w+");
+  numThreads = (argc > 3) ? atoi(argv[3]) : MAX_THREADS;
+  
+  omp_set_num_threads(numThreads);
+  
+  /* No file */
+  if (!fileToRead || !results) {
+      fprintf(stderr, "404 File not Found\n");
+      exit(1);
+    }
+
+
+  // Read dictionary
+  while(fscanf(fileToRead,"%s",dictionary[k]) == 1){
+    for(i= 0; dictionary[k][i]; i++){
+        dictionary[k][i] = tolower(dictionary[k][i]);
+    }
+    result_buffer[k] = 0;
+    k++;
+  }
+  fclose(fileToRead);
+  size = k - 1;
+
+  if(argc > 1)
+  printf("\nSearching for palindromic words in %s . . . . .\n", argv[1]);
+
+  start_time = omp_get_wtime();                  // Start time for benchmark
+  Workers(size);
+  end_time = omp_get_wtime();                    // End time for benchmar
+
+  fclose(results);
+  
+  printf("The execution took %g ms to complete\n", (end_time - start_time)*1000);
+  printf("Number of palindromic words found: %d\n", sum);
+  printf("Result stored in results.txt\n");
+
 }
