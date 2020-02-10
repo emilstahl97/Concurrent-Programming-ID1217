af8c27b94f18592210a40cde2827acba3778d16d
Emil Ståhl <emil.stahl@icloud.com>
2020-02-10 15:33:57 +0100

    make for 1k.txt

--- Homeworks/2. openMP/1. Matrix Sum/makes.sh
+++ Homeworks/2. openMP/1. Matrix Sum/makes.sh
@@ -11,7 +11,7 @@ sleep 1
 ./maxminB 1000 1000 1 >> 1k.txt
 
 echo " " >> 1k.txt
-echo "2 thread" >> 1k.txt 
+echo "2 threads" >> 1k.txt 
 sleep 1
 ./maxminB 1000 1000 2 >> 1k.txt
 sleep 1
@@ -24,7 +24,7 @@ sleep 1
 ./maxminB 1000 1000 2 >> 1k.txt
 sleep 1
 echo " " >> 1k.txt
-echo "4 thread" >> 1k.txt 
+echo "4 threads" >> 1k.txt 
 sleep 1
 ./maxminB 1000 1000 4 >> 1k.txt
 sleep 1
@@ -37,7 +37,7 @@ sleep 1
 ./maxminB 1000 1000 4 >> 1k.txt
 sleep 1
 echo " " >> 1k.txt
-echo "8 thread" >> 1k.txt 
+echo "8 threads" >> 1k.txt 
 sleep 1
 ./maxminB 1000 1000 8 >> 1k.txt
 sleep 1
@@ -50,7 +50,7 @@ sleep 1
 ./maxminB 1000 1000 8 >> 1k.txt
 sleep 1
 echo " " >> 1k.txt
-echo "16 thread" >> 1k.txt 
+echo "16 threads" >> 1k.txt 
 sleep 1
 ./maxminB 1000 1000 16 >> 1k.txt
 sleep 1
