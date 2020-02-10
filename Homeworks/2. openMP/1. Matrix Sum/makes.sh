cc79ab5d65cb17d661b9bbd1d78a21c3a7f34698
Emil Ståhl <emil.stahl@icloud.com>
2020-02-10 15:32:54 +0100

    make.sh for 1k.txt

---
+++ Homeworks/2. openMP/1. Matrix Sum/makes.sh
@@ -0,0 +1,66 @@
+echo "1 thread" >> 1k.txt 
+
+./maxminB 1000 1000 1 >> 1k.txt
+sleep 1
+./maxminB 1000 1000 1 >> 1k.txt
+sleep 1
+./maxminB 1000 1000 1 >> 1k.txt
+sleep 1
+./maxminB 1000 1000 1 >> 1k.txt
+sleep 1
+./maxminB 1000 1000 1 >> 1k.txt
+
+echo " " >> 1k.txt
+echo "2 thread" >> 1k.txt 
+sleep 1
+./maxminB 1000 1000 2 >> 1k.txt
+sleep 1
+./maxminB 1000 1000 2 >> 1k.txt
+sleep 1
+./maxminB 1000 1000 2 >> 1k.txt
+sleep 1
+./maxminB 1000 1000 2 >> 1k.txt
+sleep 1
+./maxminB 1000 1000 2 >> 1k.txt
+sleep 1
+echo " " >> 1k.txt
+echo "4 thread" >> 1k.txt 
+sleep 1
+./maxminB 1000 1000 4 >> 1k.txt
+sleep 1
+./maxminB 1000 1000 4 >> 1k.txt
+sleep 1
+./maxminB 1000 1000 4 >> 1k.txt
+sleep 1
+./maxminB 1000 1000 4 >> 1k.txt
+sleep 1
+./maxminB 1000 1000 4 >> 1k.txt
+sleep 1
+echo " " >> 1k.txt
+echo "8 thread" >> 1k.txt 
+sleep 1
+./maxminB 1000 1000 8 >> 1k.txt
+sleep 1
+./maxminB 1000 1000 8 >> 1k.txt
+sleep 1
+./maxminB 1000 1000 8 >> 1k.txt
+sleep 1
+./maxminB 1000 1000 8 >> 1k.txt
+sleep 1
+./maxminB 1000 1000 8 >> 1k.txt
+sleep 1
+echo " " >> 1k.txt
+echo "16 thread" >> 1k.txt 
+sleep 1
+./maxminB 1000 1000 16 >> 1k.txt
+sleep 1
+./maxminB 1000 1000 16 >> 1k.txt
+sleep 1
+./maxminB 1000 1000 16 >> 1k.txt
+sleep 1
+./maxminB 1000 1000 16 >> 1k.txt
+sleep 1
+./maxminB 1000 1000 16 >> 1k.txt
+sleep 1
+
+exit 1
