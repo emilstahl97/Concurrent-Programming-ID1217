b10455b1154c52102369f9d58524a9a85f55082d
Emil Ståhl <emil.stahl@icloud.com>
2020-02-10 15:53:12 +0100

    Update makes.sh

--- Homeworks/2. openMP/1. Matrix Sum/makes.sh
+++ Homeworks/2. openMP/1. Matrix Sum/makes.sh
@@ -1,5 +1,5 @@
 echo "1 thread" >> 10k.txt 
-
+echo "running first job"
 ./maxminB 10000 1000 1 >> 10k.txt
 sleep 1
 ./maxminB 10000 1000 1 >> 10k.txt
@@ -10,6 +10,7 @@ sleep 1
 sleep 1
 ./maxminB 10000 1000 1 >> 10k.txt
 
+echo "running second job"
 echo " " >> 10k.txt
 echo "2 threads" >> 10k.txt 
 sleep 1
@@ -23,6 +24,8 @@ sleep 1
 sleep 1
 ./maxminB 10000 1000 2 >> 10k.txt
 sleep 1
+
+echo "running third job"
 echo " " >> 10k.txt
 echo "4 threads" >> 10k.txt 
 sleep 1
@@ -36,6 +39,8 @@ sleep 1
 sleep 1
 ./maxminB 10000 1000 4 >> 10k.txt
 sleep 1
+
+echo "running fourth job"
 echo " " >> 10k.txt
 echo "8 threads" >> 10k.txt 
 sleep 1
@@ -49,6 +54,8 @@ sleep 1
 sleep 1
 ./maxminB 10000 1000 8 >> 10k.txt
 sleep 1
+
+echo "running fifth job"
 echo " " >> 10k.txt
 echo "16 threads" >> 10k.txt 
 sleep 1
