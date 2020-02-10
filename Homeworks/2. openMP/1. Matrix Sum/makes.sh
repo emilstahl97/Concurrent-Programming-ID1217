2d97d3b844a81f54f1af526d92a6ae9c43ab4278
Emil Ståhl <emil.stahl@icloud.com>
2020-02-10 15:49:15 +0100

    ready for 10k.txt

--- Homeworks/2. openMP/1. Matrix Sum/makes.sh
+++ Homeworks/2. openMP/1. Matrix Sum/makes.sh
@@ -1,66 +1,66 @@
-echo "1 thread" >> 1k.txt 
+echo "1 thread" >> 10k.txt 
 
-./maxminB 1000 1000 1 >> 1k.txt
+./maxminB 10000 1000 1 >> 10k.txt
 sleep 1
-./maxminB 1000 1000 1 >> 1k.txt
+./maxminB 10000 1000 1 >> 10k.txt
 sleep 1
-./maxminB 1000 1000 1 >> 1k.txt
+./maxminB 10000 1000 1 >> 10k.txt
 sleep 1
-./maxminB 1000 1000 1 >> 1k.txt
+./maxminB 10000 1000 1 >> 10k.txt
 sleep 1
-./maxminB 1000 1000 1 >> 1k.txt
+./maxminB 10000 1000 1 >> 10k.txt
 
-echo " " >> 1k.txt
-echo "2 threads" >> 1k.txt 
+echo " " >> 10k.txt
+echo "2 threads" >> 10k.txt 
 sleep 1
-./maxminB 1000 1000 2 >> 1k.txt
+./maxminB 10000 1000 2 >> 10k.txt
 sleep 1
-./maxminB 1000 1000 2 >> 1k.txt
+./maxminB 10000 1000 2 >> 10k.txt
 sleep 1
-./maxminB 1000 1000 2 >> 1k.txt
+./maxminB 10000 1000 2 >> 10k.txt
 sleep 1
-./maxminB 1000 1000 2 >> 1k.txt
+./maxminB 10000 1000 2 >> 10k.txt
 sleep 1
-./maxminB 1000 1000 2 >> 1k.txt
+./maxminB 10000 1000 2 >> 10k.txt
 sleep 1
-echo " " >> 1k.txt
-echo "4 threads" >> 1k.txt 
+echo " " >> 10k.txt
+echo "4 threads" >> 10k.txt 
 sleep 1
-./maxminB 1000 1000 4 >> 1k.txt
+./maxminB 10000 1000 4 >> 10k.txt
 sleep 1
-./maxminB 1000 1000 4 >> 1k.txt
+./maxminB 10000 1000 4 >> 10k.txt
 sleep 1
-./maxminB 1000 1000 4 >> 1k.txt
+./maxminB 10000 1000 4 >> 10k.txt
 sleep 1
-./maxminB 1000 1000 4 >> 1k.txt
+./maxminB 10000 1000 4 >> 10k.txt
 sleep 1
-./maxminB 1000 1000 4 >> 1k.txt
+./maxminB 10000 1000 4 >> 10k.txt
 sleep 1
-echo " " >> 1k.txt
-echo "8 threads" >> 1k.txt 
+echo " " >> 10k.txt
+echo "8 threads" >> 10k.txt 
 sleep 1
-./maxminB 1000 1000 8 >> 1k.txt
+./maxminB 10000 1000 8 >> 10k.txt
 sleep 1
-./maxminB 1000 1000 8 >> 1k.txt
+./maxminB 10000 1000 8 >> 10k.txt
 sleep 1
-./maxminB 1000 1000 8 >> 1k.txt
+./maxminB 10000 1000 8 >> 10k.txt
 sleep 1
-./maxminB 1000 1000 8 >> 1k.txt
+./maxminB 10000 1000 8 >> 10k.txt
 sleep 1
-./maxminB 1000 1000 8 >> 1k.txt
+./maxminB 10000 1000 8 >> 10k.txt
 sleep 1
-echo " " >> 1k.txt
-echo "16 threads" >> 1k.txt 
+echo " " >> 10k.txt
+echo "16 threads" >> 10k.txt 
 sleep 1
-./maxminB 1000 1000 16 >> 1k.txt
+./maxminB 10000 1000 16 >> 10k.txt
 sleep 1
-./maxminB 1000 1000 16 >> 1k.txt
+./maxminB 10000 1000 16 >> 10k.txt
 sleep 1
-./maxminB 1000 1000 16 >> 1k.txt
+./maxminB 10000 1000 16 >> 10k.txt
 sleep 1
-./maxminB 1000 1000 16 >> 1k.txt
+./maxminB 10000 1000 16 >> 10k.txt
 sleep 1
-./maxminB 1000 1000 16 >> 1k.txt
+./maxminB 10000 1000 16 >> 10k.txt
 sleep 1
 
 exit 1
