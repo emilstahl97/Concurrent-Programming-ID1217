5f9010b2cca30c8cc0c7d03c344fd05217386b85
Emil Ståhl <emil.stahl@icloud.com>
2020-02-10 16:27:16 +0100

    Update makes.sh

--- Homeworks/2. openMP/1. Matrix Sum/makes.sh
+++ Homeworks/2. openMP/1. Matrix Sum/makes.sh
@@ -1,73 +1,73 @@
-echo "1 thread" >> 10k.txt.txt 
+echo "1 thread" >> 10k.txt 
 echo "running first job"
-./test 10000 1000 1 >> 10k.txt.txt
+./test 10000 1000 1 >> 10k.txt
 sleep 1
-./test 10000 1000 1 >> 10k.txt.txt
+./test 10000 1000 1 >> 10k.txt
 sleep 1
-./test 10000 1000 1 >> 10k.txt.txt
+./test 10000 1000 1 >> 10k.txt
 sleep 1
-./test 10000 1000 1 >> 10k.txt.txt
+./test 10000 1000 1 >> 10k.txt
 sleep 1
-./test 10000 1000 1 >> 10k.txt.txt
+./test 10000 1000 1 >> 10k.txt
 
 echo "running second job"
-echo " " >> 10k.txt.txt
-echo "2 threads" >> 10k.txt.txt 
+echo " " >> 10k.txt
+echo "2 threads" >> 10k.txt 
 sleep 1
-./test 10000 1000 2 >> 10k.txt.txt
+./test 10000 1000 2 >> 10k.txt
 sleep 1
-./test 10000 1000 2 >> 10k.txt.txt
+./test 10000 1000 2 >> 10k.txt
 sleep 1
-./test 10000 1000 2 >> 10k.txt.txt
+./test 10000 1000 2 >> 10k.txt
 sleep 1
-./test 10000 1000 2 >> 10k.txt.txt
+./test 10000 1000 2 >> 10k.txt
 sleep 1
-./test 10000 1000 2 >> 10k.txt.txt
+./test 10000 1000 2 >> 10k.txt
 sleep 1
 
 echo "running third job"
-echo " " >> 10k.txt.txt
-echo "4 threads" >> 10k.txt.txt 
+echo " " >> 10k.txt
+echo "4 threads" >> 10k.txt 
 sleep 1
-./test 10000 1000 4 >> 10k.txt.txt
+./test 10000 1000 4 >> 10k.txt
 sleep 1
-./test 10000 1000 4 >> 10k.txt.txt
+./test 10000 1000 4 >> 10k.txt
 sleep 1
-./test 10000 1000 4 >> 10k.txt.txt
+./test 10000 1000 4 >> 10k.txt
 sleep 1
-./test 10000 1000 4 >> 10k.txt.txt
+./test 10000 1000 4 >> 10k.txt
 sleep 1
-./test 10000 1000 4 >> 10k.txt.txt
+./test 10000 1000 4 >> 10k.txt
 sleep 1
 
 echo "running fourth job"
-echo " " >> 10k.txt.txt
-echo "8 threads" >> 10k.txt.txt 
+echo " " >> 10k.txt
+echo "8 threads" >> 10k.txt 
 sleep 1
-./test 10000 1000 8 >> 10k.txt.txt
+./test 10000 1000 8 >> 10k.txt
 sleep 1
-./test 10000 1000 8 >> 10k.txt.txt
+./test 10000 1000 8 >> 10k.txt
 sleep 1
-./test 10000 1000 8 >> 10k.txt.txt
+./test 10000 1000 8 >> 10k.txt
 sleep 1
-./test 10000 1000 8 >> 10k.txt.txt
+./test 10000 1000 8 >> 10k.txt
 sleep 1
-./test 10000 1000 8 >> 10k.txt.txt
+./test 10000 1000 8 >> 10k.txt
 sleep 1
 
 echo "running fifth job"
-echo " " >> 10k.txt.txt
-echo "16 threads" >> 10k.txt.txt 
+echo " " >> 10k.txt
+echo "16 threads" >> 10k.txt 
 sleep 1
-./test 10000 1000 16 >> 10k.txt.txt
+./test 10000 1000 16 >> 10k.txt
 sleep 1
-./test 10000 1000 16 >> 10k.txt.txt
+./test 10000 1000 16 >> 10k.txt
 sleep 1
-./test 10000 1000 16 >> 10k.txt.txt
+./test 10000 1000 16 >> 10k.txt
 sleep 1
-./test 10000 1000 16 >> 10k.txt.txt
+./test 10000 1000 16 >> 10k.txt
 sleep 1
-./test 10000 1000 16 >> 10k.txt.txt
+./test 10000 1000 16 >> 10k.txt
 sleep 1
 
 exit 1
