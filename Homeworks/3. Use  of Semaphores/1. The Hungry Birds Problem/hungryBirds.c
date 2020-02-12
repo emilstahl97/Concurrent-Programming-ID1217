992703728e99a3af0a04808b2efee555884fb837
Emil Ståhl <emil.stahl@icloud.com>
2020-02-12 16:46:33 +0100

    works

--- Homeworks/3. Use  of Semaphores/1. The Hungry Birds Problem/hungryBirds.c
+++ Homeworks/3. Use  of Semaphores/1. The Hungry Birds Problem/hungryBirds.c
@@ -23,13 +23,10 @@ Usage in Linux:
 #define MAX_WORMS 50
 #define SHARED 1
 
-//int numWorms;			// current number of numWorms in dish
+int numWorms;			// current number of numWorms in dish
 int consumed[MAX_BIRDS];	// how many numWorms each bird has consumed
-int food;
 
-sem_t parent;
-sem_t baby;
-sem_t chirp;
+sem_t baby, parent, chirp;
 
 /* refills dish when it's empty */
 void *parent_bird(){
@@ -37,9 +34,9 @@ void *parent_bird(){
  while(1){
 	/* waits to be signaled */
 	sem_wait(&chirp);
-    printf("Parent bird on hunt for more worms\n");
-	food = rand()%10+1;
-    printf("%d worms found\n", food);
+	printf("Parent on hunt for more worms\n");
+	numWorms = rand()%10+1;
+    printf("%d worms found\n", numWorms);
 	sem_post(&parent);
 	sleep(1);
  }
@@ -49,40 +46,40 @@ void *parent_bird(){
 /* eats worm. signals parent when dish is empty */
 void *baby_bird(void *arg){
 
- long id = (long) arg;
+ int id = (long) arg;
 
  while(1){
-	sem_wait(&baby);				// lock to access dish	
-  		if(food == 0){
-		/* dish is empty : signal parent */
-		printf("baby bird %ld. waking parent bird\n", id);
-		sem_post(&chirp);
-		sem_wait(&parent);
+	 
+	 sem_wait(&baby);
+	 if(numWorms == 0) {
+		 printf("Bird %d chirps for food!\n", id);
+		 sem_post(&chirp);
+		 sem_wait(&parent);
+	 }
+	 numWorms--;
+	 printf("Bird %d eats worm %d \n", id, numWorms);
+	 sem_post(&baby);
+	 sleep(1);
 
-	}  
-	food--;
-	printf("Bird %d eats worm %d\n", id, food);
-	sem_post(&baby);
-	sleep(1);
  }
-
-}
+ 
+ }
 
 int main(int argc, char *argv[]) {
     
     int numBirds, id;
     
     numBirds = (argc > 1) ? atoi(argv[1]) : MAX_BIRDS;
-    food = (argc > 2) ? atoi(argv[2]) : MAX_WORMS;
-
-    printf("in main worms =%d\n",food);
+    numWorms = (argc > 2) ? atoi(argv[2]) : MAX_WORMS;
     
  pthread_t birds[numBirds];
  pthread_t parent;
 
-  sem_init(&parent, SHARED, 0);  /* sem empty = 1 */
+/* initiate semaphores */
+sem_init(&parent, SHARED, 0);  /* sem empty = 1 */
   sem_init(&baby, SHARED, 1);   /* sem full = 0  */
   sem_init(&chirp, SHARED, 0);   /* sem full = 0  */
+
 
 /* creating threads to represent birds */
  pthread_create(&parent, NULL, parent_bird, NULL);
