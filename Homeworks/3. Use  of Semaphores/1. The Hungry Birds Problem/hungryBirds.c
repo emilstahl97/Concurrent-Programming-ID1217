448aa5bcd155506e31dddeaf98303f31f0d51b3a
Emil Ståhl <emil.stahl@icloud.com>
2020-02-12 18:32:33 +0100

    works

--- Homeworks/3. Use  of Semaphores/1. The Hungry Birds Problem/hungryBirds.c
+++ Homeworks/3. Use  of Semaphores/1. The Hungry Birds Problem/hungryBirds.c
@@ -7,17 +7,17 @@
 
 #define MAX_BIRDS 10
 #define MAX_WORMS 50
-#define REFILL 10
+#define MAX_REFILL numBirds*2
 #define SHARED 1
 
 sem_t empty, full;
-int worms;
+int worms, refill;
 
 void *parent_bird() {
 	while(1) {
 		sem_wait(&empty);
 		if(worms == 0);
-		worms = REFILL;
+		worms = rand()%refill;
 		printf("---------------Parent: Refilled with %d new worms---------------\n", worms);
 		sleep(1);
 		sem_post(&full);
@@ -30,11 +30,11 @@ void *baby_bird(void *arg) {
 		sem_wait(&full);
 		if (worms > 0) {
 			worms--;
-			printf("Bird %d ate from the worms(%d left)\n", id, worms);
+			printf("Bird %d ate worm %d\n", id, worms);
 			sem_post(&full);
 			sleep(1);
 		} else {
-			printf("---------------Bird %d SQUEELS---------------\n", id);
+			printf("Bird %d SQUEELS!!!!!!!!!!!!!!!!!!!!!\n", id);
 			sleep(rand()%3);
 			sem_post(&empty);
 		}
@@ -43,32 +43,30 @@ void *baby_bird(void *arg) {
 
 
 int main(int argc, char *argv[]) {
-
-
-
-    int numBirds, id;
+	
+	int numBirds, id;
     
     numBirds = (argc > 1) ? atoi(argv[1]) : MAX_BIRDS;
     worms = (argc > 2) ? atoi(argv[2]) : MAX_WORMS;
-    
- pthread_t birds[numBirds];
- pthread_t parent;
-
-/* initiate semaphores */
- sem_init(&empty, SHARED, 0); 		// worms starts out as full
- sem_init(&full, SHARED, 1);			// mutex initialized to 1 to indicate critical section is free  	
-
-/* creating threads to represent birds */
- pthread_create(&parent, NULL, parent_bird, NULL);
-
- for(id = 0; id < numBirds; id++)
-	pthread_create(&birds[id], NULL, baby_bird, (void*)id);
-
-/* wait for threads to terminate (which they, incidentally, won't) */
- pthread_join(parent, NULL);
- for(id = 0; id < numBirds; id++)
- 	pthread_join(birds[id], NULL);
-
- return 0;
+	refill = (argc > 3) ? atoi(argv[2]) : MAX_REFILL;
+	
+	pthread_t birds[numBirds];
+	pthread_t parent;
+	
+	/* initiate semaphores */
+	sem_init(&empty, SHARED, 0); 			// worms starts out as full
+	sem_init(&full, SHARED, 1);				// mutex initialized to 1 to indicate critical section is free  	
+	
+	/* creating threads to represent birds */
+	for(id = 0; id < numBirds; id++)
+		pthread_create(&birds[id], NULL, baby_bird, (void*)id);
+	pthread_create(&parent, NULL, parent_bird, NULL);
+	
+	/* wait for threads to terminate (which they, incidentally, won't) */
+	for(id = 0; id < numBirds; id++)
+		pthread_join(birds[id], NULL);
+	pthread_join(parent, NULL);
+	
+	return 0;
 
 }
