e770cb98cfceb2470f0f06af3635d7fcafa972b2
Emil Ståhl <emil.stahl@icloud.com>
2020-02-12 17:27:12 +0100

    works

--- Homeworks/3. Use  of Semaphores/1. The Hungry Birds Problem/hungryBirds.c
+++ Homeworks/3. Use  of Semaphores/1. The Hungry Birds Problem/hungryBirds.c
@@ -1,72 +1,50 @@
-/*Hungry birds problem: one parent_bird, multiple consumers
 
-Description:
-n baby birds, one parent bird. the baby birds eat out of
-a common dish containing W numWorms. when the dish is empty,
-the parent bird is signaled and refills it. only semaphores
-are used for mutual exclusion and signaling.
-
-Usage in Linux:
-
-	gcc hungrybirds.c -lpthread
-	./a.out n
-*/
-
+#include <stdlib.h>
+#include <stdio.h>
 #include <pthread.h>
 #include <semaphore.h>
-#include <stdlib.h>
 #include <unistd.h>
-#include <stdio.h>
-
 
 #define MAX_BIRDS 10
 #define MAX_WORMS 50
 #define SHARED 1
 
-int numWorms;			// current number of numWorms in dish
-int consumed[MAX_BIRDS];	// how many numWorms each bird has consumed
+sem_t empty, full;
+int numWorms, numBabyBirds;
+int worms;
 
-sem_t baby, parent, chirp;
+void *parent_bird() {
+	while(1) {
+		sem_wait(&empty);
+		worms = numWorms;
+		printf("---------------Parent: Refilled worms---------------\n");
+		sleep(1);
+		sem_post(&full);
+	}
+}
 
-/* refills dish when it's empty */
-void *parent_bird(){
-
- while(1){
-	/* waits to be signaled */
-	sem_wait(&chirp);
-	printf("Parent on hunt for more worms\n");
-	numWorms = rand()%10+1;
-    printf("%d worms found\n", numWorms);
-	sem_post(&parent);
-	sleep(1);
- }
+void *baby_bird(void *arg) {
+	int id = (int) arg;
+	while(1) {
+		sem_wait(&full);
+		if (worms > 0) {
+			worms--;
+			printf("Bird %d ate from the worms(%d left)\n", id, worms);
+			sem_post(&full);
+			sleep(1);
+		} else {
+			printf("---------------Bird %d SQUEELS---------------\n", id);
+			sleep(1);
+			sem_post(&empty);
+		}
+	}
 }
 
 
-/* eats worm. signals parent when dish is empty */
-void *baby_bird(void *arg){
-
- int id = (long) arg;
-
- while(1){
-	 
-	 sem_wait(&baby);
-	 if(numWorms == 0) {
-		 printf("Bird %d chirps for food!\n", id);
-		 sem_post(&chirp);
-		 sem_wait(&parent);
-	 }
-	 numWorms--;
-	 printf("Bird %d eats worm %d \n", id, numWorms);
-	 sem_post(&baby);
-	 sleep(1);
-
- }
- 
- }
-
 int main(int argc, char *argv[]) {
-    
+
+
+
     int numBirds, id;
     
     numBirds = (argc > 1) ? atoi(argv[1]) : MAX_BIRDS;
@@ -76,10 +54,8 @@ int main(int argc, char *argv[]) {
  pthread_t parent;
 
 /* initiate semaphores */
-sem_init(&parent, SHARED, 0);  /* sem empty = 1 */
-  sem_init(&baby, SHARED, 1);   /* sem full = 0  */
-  sem_init(&chirp, SHARED, 0);   /* sem full = 0  */
-
+ sem_init(&empty, SHARED, 0); 		// worms starts out as full
+ sem_init(&full, SHARED, 1);			// mutex initialized to 1 to indicate critical section is free  	
 
 /* creating threads to represent birds */
  pthread_create(&parent, NULL, parent_bird, NULL);
