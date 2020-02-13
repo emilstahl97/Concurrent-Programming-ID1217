7683eb129b38d60b050bca819fcd351aa6453c11
Emil Ståhl <emil.stahl@icloud.com>
2020-02-13 17:56:13 +0100

    works

--- Homeworks/3. Use of Semaphores/2. The Bear and Honeybees Problem/honeyBees.c
+++ Homeworks/3. Use of Semaphores/2. The Bear and Honeybees Problem/honeyBees.c
@@ -1,11 +1,11 @@
-/*  This program simulates a consuumer/producer with birds. The program initiates a number of baby birds and a parent bird.
-	The baby birds eat out of a common dish that initially contains W worms. Each baby bird repeatedly takes a worm, eats it, sleeps for a while, takes another worm, and so on. 
-	If the dish is empty, the baby bird who discovers the empty dish chirps real loud to awaken the parent bird. The parent bird flies off and gathers W more worms, puts them in the dish, and then waits for the dish to be empty again. 
+/*  This program simulates a consuumer/producer with bees. The program initiates a number of baby bees and a bear bird.
+	The baby bees eat out of a common dish that initially contains W worms. Each baby bird repeatedly takes a worm, eats it, sleeps for a while, takes another worm, and so on. 
+	If the dish is wakeUp, the baby bird who discovers the wakeUp dish chirps real loud to awaken the bear bird. The bear bird flies off and gathers W more worms, puts them in the dish, and then waits for the dish to be wakeUp again. 
 	This pattern repeats forever.
     
    usage under Linux:
             gcc hungryBirds.c -o hungryBird -lpthread
-            ./hungryBirds numBirds numWorms
+            ./hungryBirds numBees numWorms
 */
 
 #include <stdlib.h>
@@ -14,70 +14,64 @@
 #include <semaphore.h>
 
 #define MAX_SLEEP 10
-#define MAX_BIRDS 10
-#define MAX_WORMS 50
-#define MAX_REFILL numBirds*2
+#define MAX_BEES 10
+#define FULL 50
+#define EATEN 0
 #define SHARED 1
 
-sem_t empty, full;
-int worms, refill, sleepInterval;
+sem_t wakeUp, empty;
+int honeyPot = 0;
 
-void *baby_bird(void *arg) {
+void *bee(void *arg) {
 	int id = (int) arg;
 	while(1) {
-		sem_wait(&full);
-		if (worms > 0) {
-			worms--;
-			printf("Bird %d ate worm %d\n", id, worms);
-			sem_post(&full);
-			sleep(rand()%sleepInterval);
-			printf("Bird %d sleeps\n",id);
-		} else {
-			printf("Bird %d SQUEELS!!!!!!!!!!!!!!!!!!!!!\n", id);
-			sleep(rand()%sleepInterval);
-			sem_post(&empty);
-		}
+		sem_wait(&empty);
+        honeyPot++;
+        printf("Bee nr %d added some honey to the pot = %d\n", id, honeyPot);
+        if(honeyPot == FULL) {
+            printf("Honeypot is full! Bee nr %d waking up the bear\n", id);
+            sem_post(&wakeUp);
+            sleep(1);
+        }
+        else {
+            sem_post(&empty);
+            sleep(1);
+        }
 	}
 }
 
-void *parent_bird() {
+void *bear() {
 	while(1) {
-		sem_wait(&empty);
-		if(worms == 0);
-		worms = rand()%refill;
-		printf("**********Parent bird awake**********************\n");
-		printf("**********Parent bird found %d new worms*********\n", worms);
-		printf("**********Parent bird sleeping*******************\n");
-		sleep(1);
-		sem_post(&full);
+		sem_wait(&wakeUp);
+        printf("Bear awake!\n");
+        printf("The bear ate all the honeey and is now going back to sleep\n");
+		honeyPot = EATEN;
+        sleep(1);
+		sem_post(&empty);
 	}
 }
 
 int main(int argc, char *argv[]) {
 	
-	int numBirds, id;
+	int numBees, id;
     
-    numBirds = (argc > 1) ? atoi(argv[1]) : MAX_BIRDS;
-    worms = (argc > 2) ? atoi(argv[2]) : MAX_WORMS;
-	refill = (argc > 3) ? atoi(argv[2]) : MAX_REFILL;
-	sleepInterval = (argc > 4) ? atoi(argv[4]) : MAX_SLEEP;
+    numBees = (argc > 1) ? atoi(argv[1]) : MAX_BEES;
 	
-	pthread_t birds[numBirds];
-	pthread_t parent;
+	pthread_t bearT, bees[numBees];
 	
 	/* initiate semaphores */
-	sem_init(&empty, SHARED, 0); 			// worms starts out as full
-	sem_init(&full, SHARED, 1);				// mutex initialized to 1 to indicate critical section is free  	
+	sem_init(&wakeUp, SHARED, 0); 			// worms starts out as empty
+	sem_init(&empty, SHARED, 1);				// mutex initialized to 1 to indicate critical section is free  	
 	
-	/* creating threads to represent birds */
-	for(id = 0; id < numBirds; id++)
-		pthread_create(&birds[id], NULL, baby_bird, (void*)id);
-	pthread_create(&parent, NULL, parent_bird, NULL);
+	/* creating threads to represent bees */
+	for(id = 0; id < numBees; id++)
+		pthread_create(&bees[id], NULL, bee, (void*)id);
+	pthread_create(&bearT, NULL, bear, NULL);
 	
 	/* wait for threads to terminate (which they, incidentally, won't) */
-	for(id = 0; id < numBirds; id++)
-		pthread_join(birds[id], NULL);
-	pthread_join(parent, NULL);
+	for(id = 0; id < numBees; id++)
+		pthread_join(bees[id], NULL);
+	pthread_join(bearT, NULL);
 	
 	return 0;
 
