9acdbd0f9a6eab25ccaebcf276c7f34a07ca0d9c
Emil Ståhl <emil.stahl@icloud.com>
2020-02-17 14:58:43 +0100

    removed unused defines

--- Homeworks/3. Use of Semaphores/3. The Unisex Bathroom Problem/config.h
+++ Homeworks/3. Use of Semaphores/3. The Unisex Bathroom Problem/config.h
@@ -13,11 +13,9 @@
 #define SHARED 0        /* The semaphores should not be shared. */
 #define MAX_MEN 10     /* The max number of males possible. */
 #define MAX_WOMEN 10     /* The max number of women possible. */
-#define MAXTIMES 20     /* Max number of times the bathroom is used. */
-#define MAXTIMEIN 3     /* Max seconds in the bathroom. */
-#define MINTIMEIN 1     /* Min seconds in the bathroom. */
-#define MAXINTERVAL 5   /* Max time between visits to the bathroom. */
-#define MININTERVAL 1   /* Min time between visits to the bathroom. */
+#define MAX_VISITS 20     /* Max number of times the bathroom is used. */
+#define MAX_TIMEIN 3     /* Max seconds in the bathroom. */
+#define MAX_INTERVAL 5   /* Max time between visits to the bathroom. */
 
 #define GRN "\x1B[32m"
 #define YEL "\x1B[33m"
