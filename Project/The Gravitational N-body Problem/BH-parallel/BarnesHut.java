c46a593dbe275162fed7dad66a70132900faa596
Emil Ståhl <emil.stahl@icloud.com>
2020-03-06 18:52:32 +0100

    works

--- Project/The Gravitational N-body Problem/BH-parallel/BarnesHut.java
+++ Project/The Gravitational N-body Problem/BH-parallel/BarnesHut.java
@@ -2,15 +2,15 @@
 import java.util.Random;
 import java.util.concurrent.CyclicBarrier;
 
-
 public class BarnesHut {
 
     public static int MAX_WORKERS = 20;
     public static int MAX_BODIES = 100;
     public static int MAX_STEPS = 300000;
-    public static double maxlength;
     public static int gnumBodies;
-
+    public static int numSteps;
+    public static int numWorkers;
+    public static double maxlength;
 
     Point[] points;
 
@@ -38,8 +38,8 @@ public class BarnesHut {
             System.out.println("Executing with default arguments:\n");
 
         gnumBodies = (args.length > 0) && (Integer.parseInt(args[0]) < MAX_BODIES) ? Integer.parseInt(args[0]) : MAX_BODIES;
-        int numSteps = (args.length > 1) && (Integer.parseInt(args[1]) < MAX_STEPS) ? Integer.parseInt(args[1]) : MAX_STEPS;
-        int numWorkers = (args.length > 2) && (Integer.parseInt(args[2]) < MAX_WORKERS) ? Integer.parseInt(args[2]) : MAX_WORKERS;
+        numSteps = (args.length > 1) && (Integer.parseInt(args[1]) < MAX_STEPS) ? Integer.parseInt(args[1]) : MAX_STEPS;
+        numWorkers = (args.length > 2) && (Integer.parseInt(args[2]) < MAX_WORKERS) ? Integer.parseInt(args[2]) : MAX_WORKERS;
 
         System.out.println("gnumBodies = " + gnumBodies + "\nnumSteps = " + numSteps + "\nnumWorkers = " + numWorkers + "\n");
 
@@ -50,7 +50,5 @@ public class BarnesHut {
             Worker worker = new Worker(i, numSteps, gnumBodies, numWorkers, maxlength, simulation, barrier);
             worker.start();
         }
-
     }
-
 }
