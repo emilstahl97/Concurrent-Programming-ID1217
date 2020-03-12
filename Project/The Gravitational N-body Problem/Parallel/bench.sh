9cf84c953ded99fc736d7236ca97cc699ed4e673
Emil Ståhl <emil.stahl@icloud.com>
2020-03-12 16:56:03 +0100

    ready for 240

--- Project/The Gravitational N-body Problem/Parallel/bench.sh
+++ Project/The Gravitational N-body Problem/Parallel/bench.sh
@@ -1,89 +1,89 @@
 javac ParallelNbody.java
-echo "Running parallel with 180 bodies"
+echo "Running parallel with 240 bodies"
 
-echo "1 thread" >> parallel-180.dat
+echo "1 thread" >> parallel-240.dat
 
 echo "1 thread"
 echo "Running execution 1"
-java ParallelNbody 180 56500 1 >> parallel-180.dat
+java ParallelNbody 240 56500 1 >> parallel-240.dat
 sleep 1
 echo "Running execution 2"
-java ParallelNbody 180 56500 1 >> parallel-180.dat
+java ParallelNbody 240 56500 1 >> parallel-240.dat
 sleep 1
 echo "Running execution 3"
-java ParallelNbody 180 56500 1 >> parallel-180.dat
+java ParallelNbody 240 56500 1 >> parallel-240.dat
 sleep 1
 echo "Running execution 4"
-java ParallelNbody 180 56500 1 >> parallel-180.dat
+java ParallelNbody 240 56500 1 >> parallel-240.dat
 sleep 1
 echo "Running execution 5"
-java ParallelNbody 180 56500 1 >> parallel-180.dat
+java ParallelNbody 240 56500 1 >> parallel-240.dat
 sleep 1
 
 echo ""
 echo "2 threads"
-echo "2 thread" >> parallel-180.dat
-echo "" >> parallel-180.dat
+echo "2 thread" >> parallel-240.dat
+echo "" >> parallel-240.dat
 
 
 echo "Running execution 1"
-java ParallelNbody 180 56500 2 >> parallel-180.dat
+java ParallelNbody 240 56500 2 >> parallel-240.dat
 sleep 1
 echo "Running execution 2"
-java ParallelNbody 180 56500 2 >> parallel-180.dat
+java ParallelNbody 240 56500 2 >> parallel-240.dat
 sleep 1
 echo "Running execution 3"
-java ParallelNbody 180 56500 2 >> parallel-180.dat
+java ParallelNbody 240 56500 2 >> parallel-240.dat
 sleep 1
 echo "Running execution 4"
-java ParallelNbody 180 56500 2 >> parallel-180.dat
+java ParallelNbody 240 56500 2 >> parallel-240.dat
 sleep 1
 echo "Running execution 5"
-java ParallelNbody 180 56500 2 >> parallel-180.dat
+java ParallelNbody 240 56500 2 >> parallel-240.dat
 sleep 1
 
 echo ""
 echo "3 threads"
 echo ""
 
-echo "3 thread" >> parallel-180.dat
+echo "3 thread" >> parallel-240.dat
 
 echo "Running execution 1"
-java ParallelNbody 180 56500 3 >> parallel-180.dat
+java ParallelNbody 240 56500 3 >> parallel-240.dat
 sleep 1
 echo "Running execution 2"
-java ParallelNbody 180 56500 3 >> parallel-180.dat
+java ParallelNbody 240 56500 3 >> parallel-240.dat
 sleep 1
 echo "Running execution 3"
-java ParallelNbody 180 56500 3 >> parallel-180.dat
+java ParallelNbody 240 56500 3 >> parallel-240.dat
 sleep 1
 echo "Running execution 4"
-java ParallelNbody 180 56500 3 >> parallel-180.dat
+java ParallelNbody 240 56500 3 >> parallel-240.dat
 sleep 1
 echo "Running execution 5"
-java ParallelNbody 180 56500 3 >> parallel-180.dat
+java ParallelNbody 240 56500 3 >> parallel-240.dat
 sleep 1
 
 echo ""
 echo "4 threads"
 echo ""
 
-echo "4 thread" >> parallel-180.dat
+echo "4 thread" >> parallel-240.dat
 
 echo "Running execution 1"
-java ParallelNbody 180 56500 4 >> parallel-180.dat
+java ParallelNbody 240 56500 4 >> parallel-240.dat
 sleep 1
 echo "Running execution 2"
-java ParallelNbody 180 56500 4 >> parallel-180.dat
+java ParallelNbody 240 56500 4 >> parallel-240.dat
 sleep 1
 echo "Running execution 3"
-java ParallelNbody 180 56500 4 >> parallel-180.dat
+java ParallelNbody 240 56500 4 >> parallel-240.dat
 sleep 1
 echo "Running execution 4"
-java ParallelNbody 180 56500 4 >> parallel-180.dat
+java ParallelNbody 240 56500 4 >> parallel-240.dat
 sleep 1
 echo "Running execution 5"
-java ParallelNbody 180 56500 4 >> parallel-180.dat
+java ParallelNbody 240 56500 4 >> parallel-240.dat
 sleep 1
 
 echo "done"
