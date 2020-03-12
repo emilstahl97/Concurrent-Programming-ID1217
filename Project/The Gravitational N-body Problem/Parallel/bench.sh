06d82db83bcd3a41296c6424ec146393db540615
Emil Ståhl <emil.stahl@icloud.com>
2020-03-12 16:40:41 +0100

    Update bench.sh

--- Project/The Gravitational N-body Problem/Parallel/bench.sh
+++ Project/The Gravitational N-body Problem/Parallel/bench.sh
@@ -1,7 +1,7 @@
 javac ParallelNbody.java
 echo "Running parallel with 180 bodies"
 
-echo "1 thread" >> parallel-180.data
+echo "1 thread" >> parallel-180.dat
 
 echo "1 thread"
 echo "Running execution 1"
@@ -22,7 +22,7 @@ sleep 1
 
 echo ""
 echo "2 threads"
-echo "2 thread" >> parallel-180.data
+echo "2 thread" >> parallel-180.dat
 echo "" >> parallel-180.dat
 
 
@@ -46,7 +46,7 @@ echo ""
 echo "3 threads"
 echo ""
 
-echo "3 thread" >> parallel-180.data
+echo "3 thread" >> parallel-180.dat
 
 echo "Running execution 1"
 java ParallelNbody 180 56500 3 >> parallel-180.dat
@@ -68,7 +68,7 @@ echo ""
 echo "4 threads"
 echo ""
 
-echo "4 thread" >> parallel-180.data
+echo "4 thread" >> parallel-180.dat
 
 echo "Running execution 1"
 java ParallelNbody 180 56500 4 >> parallel-180.dat
