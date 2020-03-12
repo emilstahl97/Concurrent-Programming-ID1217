3ecda6af7bf71dbab581dc21386a710b613c3e90
Emil Ståhl <emil.stahl@icloud.com>
2020-03-12 17:21:52 +0100

    ready

--- Project/The Gravitational N-body Problem/BH-Sequential/bench.sh
+++ Project/The Gravitational N-body Problem/BH-Sequential/bench.sh
@@ -3,44 +3,63 @@ javac BarnesHut.java
 
 echo "120" >> sequential-120.dat
 echo "" >> sequential-120.dat
+echo "Running 120"
 
-java BarnesHut 120 56500 >> sequential-120.dat
+echo "Running execution 1"
+java BarnesHut 120 56500 100 >> sequential-120.dat
 sleep 1
-java BarnesHut 120 56500 >> sequential-120.dat
+echo "Running execution 2"
+java BarnesHut 120 56500 100 >> sequential-120.dat
 sleep 1
-java BarnesHut 120 56500 >> sequential-120.dat
+echo "Running execution 3"
+java BarnesHut 120 56500 100 >> sequential-120.dat
 sleep 1
-java BarnesHut 120 56500 >> sequential-120.dat
+echo "Running execution 4"
+java BarnesHut 120 56500 100 >> sequential-120.dat
 sleep 1
-java BarnesHut 120 56500 >> sequential-120.dat
+echo "Running execution 5"
+java BarnesHut 120 56500 100 >> sequential-120.dat
 sleep 1
 
 echo "180" >> sequential-180.dat
 echo "" >> sequential-180.dat
+echo "180"
 
-java BarnesHut 180 56500 >> sequential-180.dat
+echo "Running execution 1"
+java BarnesHut 180 56500 100 >> sequential-180.dat
 sleep 1
-java BarnesHut 180 56500 >> sequential-180.dat
+echo "Running execution 2"
+java BarnesHut 180 56500 100 >> sequential-180.dat
 sleep 1
-java BarnesHut 180 56500 >> sequential-180.dat
+echo "Running execution 3"
+java BarnesHut 180 56500 100 >> sequential-180.dat
 sleep 1
-java BarnesHut 180 56500 >> sequential-180.dat
+echo "Running execution 4"
+java BarnesHut 180 56500 100 >> sequential-180.dat
 sleep 1
-java BarnesHut 180 56500 >> sequential-180.dat
+echo "Running execution 5"
+java BarnesHut 180 56500 100 >> sequential-180.dat
 sleep 1
+
+echo "240"
 
 echo "240" >> sequential-240.dat
 echo "" >> sequential-240.dat
 
-java BarnesHut 240 56500 >> sequential-240.dat
+echo "Running execution 1"
+java BarnesHut 240 56500 100 >> sequential-240.dat
 sleep 1
-java BarnesHut 240 56500 >> sequential-240.dat
+echo "Running execution 2"
+java BarnesHut 240 56500 100 >> sequential-240.dat
 sleep 1
-java BarnesHut 240 56500 >> sequential-240.dat
+echo "Running execution 3"
+java BarnesHut 240 56500 100 >> sequential-240.dat
 sleep 1
-java BarnesHut 240 56500 >> sequential-240.dat
+echo "Running execution 4"
+java BarnesHut 240 56500 100 >> sequential-240.dat
 sleep 1
-java BarnesHut 240 56500 >> sequential-240.dat
+echo "Running execution 5"
+java BarnesHut 240 56500 100 >> sequential-240.dat
 sleep 1
 
 echo "done"
