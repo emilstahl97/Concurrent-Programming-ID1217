bd1d9823ec1e6f7606a760ffebf38bf5be34b43a
Emil Ståhl <emil.stahl@icloud.com>
2020-03-12 15:37:35 +0100

    ready for 3

--- Project/The Gravitational N-body Problem/Parallel/bench.sh
+++ Project/The Gravitational N-body Problem/Parallel/bench.sh
@@ -1,16 +1,16 @@
 javac ParallelNbody.java
 echo "Running execution 1"
-java ParallelNbody 120 56500 2 >> parallel-120.dat
+java ParallelNbody 120 56500 3 >> parallel-120.dat
 sleep 1
 echo "Running execution 2"
-java ParallelNbody 120 56500 2 >> parallel-120.dat
+java ParallelNbody 120 56500 3 >> parallel-120.dat
 sleep 1
 echo "Running execution 3"
-java ParallelNbody 120 56500 2 >> parallel-120.dat
+java ParallelNbody 120 56500 3 >> parallel-120.dat
 sleep 1
 echo "Running execution 4"
-java ParallelNbody 120 56500 2 >> parallel-120.dat
+java ParallelNbody 120 56500 3 >> parallel-120.dat
 sleep 1
 echo "Running execution 5"
-java ParallelNbody 120 56500 2 >> parallel-120.dat
+java ParallelNbody 120 56500 3 >> parallel-120.dat
 sleep 1
